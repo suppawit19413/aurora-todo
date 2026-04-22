aa5a933d094debd1b8aebe47d3fc4ac600ad366c
suppawit19413 <148687235+suppawit19413@users.noreply.github.com>
2026-04-22 14:37:29 +0700

    Deploy Aurora Todo App via MASTER_PROTOCOL

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'aurora-todo-v1';
+const ASSETS = [
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json',
+  'todo_app_icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
